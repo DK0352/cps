737e3a13cb173a41a9b10aaf1f8da0899ec15af9
Danilo Kovljanić <winterway1726@gmail.com>
2023-09-29 13:04:02 +0200

    small fixes

--- compare_trees.c
+++ compare_trees.c
@@ -191,7 +191,8 @@ int compare_trees(struct thread_struct *thread_data_a, struct thread_struct *thr
 						exit(1);
 					}
 				}
-				file_tree_element_b = file_tree_element_b->this_directory;
+				if (file_tree_element_b->this_is_top_dir != 1)
+					file_tree_element_b = file_tree_element_b->this_directory;
 				new_dir_location(file_tree_element_a,file_tree_element_b,data_copy_info.dirs_to_copy_list);
 
 				data_copy_info.global_dirs_to_copy_num++;
