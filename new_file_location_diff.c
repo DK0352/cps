2d1e95c685e72e6af3349adf589b0b020879aa18
Danilo Kovljanić <winterway1726@gmail.com>
2023-10-16 13:58:16 +0200

    timestamps 1st level

--- new_file_location_diff.c
+++ new_file_location_diff.c
@@ -19,6 +19,8 @@
 
 #include "main.h"
 #include "dlist.h"
+#include "options.h"
+extern struct options_menu options;
 
 // This function is used when two files with the same name are found, but different size or type. It just adds a file to a list to copy. It can be used to invert location in case of adding the file
 // to a surplus list, so the file is read from the destionation directory and written to the source directory
@@ -28,6 +30,10 @@ char *new_file_location_diff(DListElmt *main_location, DListElmt *new_location, 
 	char *name, *dir_location_1, *dir_location_2;
 	mode_t st_mode;
 	long size;
+	time_t atime, mtime;
+
+	atime = 0;
+	mtime = 0;
 
 	size1 = size2 = size3 = 0;
 
@@ -49,8 +55,15 @@ char *new_file_location_diff(DListElmt *main_location, DListElmt *new_location, 
 		printf("new_file_location(): strdup error 2. exiting.\n");
 		exit(1);
 	}
-
-	dlist_ins_next(insert_to,insert_to->tail,name,st_mode,size,dir_location_1,0,dir_location_2,0,0);
-
+	if (options.time_mods == 0)
+		dlist_ins_next(insert_to,insert_to->tail,name,st_mode,size,dir_location_1,0,dir_location_2,main_location->atime,main_location->mtime);
+	else if (options.time_mods == 1) {
+		if (options.preserve_a_time == 1)
+			atime = main_location->atime;
+		if (options.preserve_m_time == 1)
+			mtime = main_location->mtime;
+		dlist_ins_next(insert_to,insert_to->tail,name,st_mode,size,dir_location_1,0,dir_location_2,main_location->atime,main_location->mtime);
+	}
+	
 	return dir_location_2;
 }
