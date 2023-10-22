e1469e17312356d8fba3d3152a8977202d6c8331
Danilo Kovljanić <winterway1726@gmail.com>
2023-10-22 22:22:39 +0200

    performance update

--- create_dirs.c
+++ create_dirs.c
@@ -19,6 +19,7 @@
 
 #include "main.h"
 #include "dlist.h"
+#include "data_copy_info.h"
 
 void init_to_zero(DList_of_lists *file_tree_element);
 
@@ -26,18 +27,18 @@ void init_to_zero(DList_of_lists *file_tree_element);
  * function continously. build_tree() calls create_top_dirs just for the top directories, and then onwards create_dirs(). */
 DList_of_lists *create_top_dirs(DList *list, DList_of_lists *file_tree_element)
 {
-	DList_of_lists 		*save_old_position, *alloc, *save_first_dir, *save_this_dir;
+	DList_of_lists 		*save_old_position, *alloc, *save_first_dir, *save_file_tree_top_dir;
 	DListElmt		*dirlist_pointer;
 	int a, limit;
 	int top_dir_num;	// broj top direktorija
+	extern struct Data_Copy_Info data_copy_info;	// benchmark
 
 	limit = list->num;
 	dirlist_pointer = list->head;
 	top_dir_num = 0;
 
 	file_tree_element->down = NULL;
-	save_this_dir = file_tree_element;
-	file_tree_element->this_directory = save_this_dir;
+	save_file_tree_top_dir = file_tree_element;
 	save_old_position = file_tree_element;
 
 	for (a = 1; a <= limit; a++, dirlist_pointer = dirlist_pointer->next) {
@@ -48,6 +49,7 @@ DList_of_lists *create_top_dirs(DList *list, DList_of_lists *file_tree_element)
 				printf("error allocating memory - create_top_dirs(1)\n");
 				exit(1);
 			}
+			data_copy_info.dlist_of_lists_num++;
 			file_tree_element->next = alloc;
 			file_tree_element->first_dir_in_chain = alloc;
 			file_tree_element = file_tree_element->next;
@@ -59,13 +61,14 @@ DList_of_lists *create_top_dirs(DList *list, DList_of_lists *file_tree_element)
 			file_tree_element->one_of_the_top_dirs = file_tree_element;
 			file_tree_element->one_of_the_top_dirs_num = ++top_dir_num;
 			file_tree_element->first_dir_in_chain = save_first_dir;
-			file_tree_element->this_directory = save_this_dir;
+			file_tree_element->file_tree_top_dir = save_file_tree_top_dir;
 			file_tree_element->this_is_top_dir = 1;
 			file_tree_element->dirname = dirlist_pointer->name; /* using directory list created using open_dirs() function and creating dlist_of_lists file tree */
 			file_tree_element->dir_location = dirlist_pointer->dir_location; /* just using pointers as all the allocated data will be destroyed by using dlist_destroy function in process of destroying dlist_of_lists */
 			file_tree_element->st_mode = dirlist_pointer->st_mode;
 			file_tree_element->atime = dirlist_pointer->atime;
 			file_tree_element->mtime = dirlist_pointer->mtime;
+			dirlist_pointer->tree_position = file_tree_element;
 		}
 		// just one directory
 		if (limit == 1) {
@@ -82,6 +85,7 @@ DList_of_lists *create_top_dirs(DList *list, DList_of_lists *file_tree_element)
 				printf("error allocating memory - create_top_dirs(2)\n");
 				exit(1);
 			}
+			data_copy_info.dlist_of_lists_num++;
 			file_tree_element->next = alloc;
 			file_tree_element = file_tree_element->next;
 			init_to_zero(file_tree_element);
@@ -89,7 +93,7 @@ DList_of_lists *create_top_dirs(DList *list, DList_of_lists *file_tree_element)
 			file_tree_element->prev = save_old_position;
 			file_tree_element->one_of_the_top_dirs = file_tree_element;
 			file_tree_element->one_of_the_top_dirs_num = ++top_dir_num;
-			file_tree_element->this_directory = save_this_dir;
+			file_tree_element->file_tree_top_dir = save_file_tree_top_dir;
 			file_tree_element->first_dir_in_chain = save_first_dir;
 			file_tree_element->this_is_top_dir = 1;
 			file_tree_element->dirname = dirlist_pointer->name;
@@ -97,6 +101,8 @@ DList_of_lists *create_top_dirs(DList *list, DList_of_lists *file_tree_element)
 			file_tree_element->st_mode = dirlist_pointer->st_mode;
 			file_tree_element->atime = dirlist_pointer->atime;
 			file_tree_element->mtime = dirlist_pointer->mtime;
+			dirlist_pointer->tree_position = file_tree_element;
+
 		}
 		else if (a == limit && limit != 1) {
 			save_old_position = file_tree_element;
@@ -105,6 +111,7 @@ DList_of_lists *create_top_dirs(DList *list, DList_of_lists *file_tree_element)
 				printf("error allocating memory - create_top_dirs(3)\n");
 				exit(1);
 			}
+			data_copy_info.dlist_of_lists_num++;
 			file_tree_element->next = alloc;
 			file_tree_element = file_tree_element->next;
 			init_to_zero(file_tree_element);
@@ -113,7 +120,7 @@ DList_of_lists *create_top_dirs(DList *list, DList_of_lists *file_tree_element)
 			file_tree_element->prev = save_old_position;
 			file_tree_element->one_of_the_top_dirs = file_tree_element;
 			file_tree_element->one_of_the_top_dirs_num = ++top_dir_num;
-			file_tree_element->this_directory = save_this_dir;
+			file_tree_element->file_tree_top_dir = save_file_tree_top_dir;
 			file_tree_element->first_dir_in_chain = save_first_dir;
 			file_tree_element->this_is_top_dir = 1;
 			file_tree_element->last_dir = 1;
@@ -122,10 +129,11 @@ DList_of_lists *create_top_dirs(DList *list, DList_of_lists *file_tree_element)
 			file_tree_element->st_mode = dirlist_pointer->st_mode;
 			file_tree_element->atime = dirlist_pointer->atime;
 			file_tree_element->mtime = dirlist_pointer->mtime;
+			dirlist_pointer->tree_position = file_tree_element;
 			save_old_position = file_tree_element;
 			file_tree_element = file_tree_element->first_dir_in_chain;
 			file_tree_element->last_dir_in_chain = save_old_position;
-			file_tree_element->this_directory->last_dir_in_chain = save_old_position;
+			file_tree_element->file_tree_top_dir->last_dir_in_chain = save_old_position;
 
 			return file_tree_element;
 		}
@@ -134,16 +142,18 @@ DList_of_lists *create_top_dirs(DList *list, DList_of_lists *file_tree_element)
 /* size je dlist_num(directories), ubaci iza file_tree_element-a, dirlist_pointer je dlist_head(directories) */
 DList_of_lists *create_dirs(DList *list, DList_of_lists *file_tree_element, DList_of_lists *file_tree_top_dir)
 {
-	DList_of_lists 		*save_old_position, *save_up_position, *alloc, *save_first_dir, *save_this_dir;
+	DList_of_lists 		*save_old_position, *save_up_position, *alloc, *save_first_dir, *save_one_of_top_dirs;
 	DListElmt		*dirlist_pointer;
 	int a, limit;
+	extern struct Data_Copy_Info data_copy_info;	// benchmark
 
 	if (list != NULL) {
 		limit = list->num;
 		dirlist_pointer = list->head;
 		save_up_position = file_tree_element->up;
-		save_this_dir = file_tree_element;
-		save_this_dir->down = NULL;
+		save_first_dir = file_tree_element;
+		file_tree_element->first_dir_in_chain = file_tree_element;
+		
 	}
 	else {
 		printf("create_dirs error: list == NULL!\n");
@@ -151,28 +161,26 @@ DList_of_lists *create_dirs(DList *list, DList_of_lists *file_tree_element, DLis
 	}
 	for (a = 1; a <= limit; a++, dirlist_pointer = dirlist_pointer->next) {
 		if (a == 1) {
-			file_tree_element->this_directory = save_this_dir;
-			alloc = malloc(sizeof(DList_of_lists));
+			/*alloc = malloc(sizeof(DList_of_lists));
 			if (alloc == NULL) {
 				printf("error allocating memory - create_dirs(1)\n");
 				exit(1);
 			}
-			file_tree_element->next = alloc;
-			file_tree_element->first_dir_in_chain = alloc;
-			file_tree_element = file_tree_element->next;
+			file_tree_element->next = alloc;*/
+			//file_tree_element = file_tree_element->next;
 			// file_tree_element je sada first_dir_in_chain
-			init_to_zero(file_tree_element);
-			file_tree_element->up = save_up_position;
-			file_tree_element->prev = save_this_dir;
-			file_tree_element->one_of_the_top_dirs = save_this_dir->one_of_the_top_dirs;
-			save_first_dir = file_tree_element;
-			file_tree_element->first_dir_in_chain = save_first_dir;
-			file_tree_element->this_directory = save_this_dir;
+			//file_tree_element->up = save_up_position;
+			//file_tree_element->prev = save_this_dir;
+			file_tree_element->one_of_the_top_dirs = save_up_position->one_of_the_top_dirs;
+			//save_first_dir = file_tree_element;
+			//file_tree_element->first_dir_in_chain = save_first_dir;
+			//file_tree_element->this_directory = save_this_dir;
 			file_tree_element->dirname = dirlist_pointer->name; 
 			file_tree_element->dir_location = dirlist_pointer->dir_location;
 			file_tree_element->st_mode = dirlist_pointer->st_mode;
 			file_tree_element->atime = dirlist_pointer->atime;
 			file_tree_element->mtime = dirlist_pointer->mtime;
+			dirlist_pointer->tree_position = file_tree_element;
 			if (limit == 1) {
 				file_tree_element->last_dir = 1;
 				file_tree_element->first_dir_in_chain->last_dir_in_chain = file_tree_element;
@@ -187,19 +195,20 @@ DList_of_lists *create_dirs(DList *list, DList_of_lists *file_tree_element, DLis
 				printf("error allocating memory - create_dirs(2)\n");
 				exit(1);
 			}
+			data_copy_info.dlist_of_lists_num++;
 			file_tree_element->next = alloc;
 			file_tree_element = file_tree_element->next;
 			init_to_zero(file_tree_element);
 			file_tree_element->up = save_up_position;
 			file_tree_element->prev = save_old_position;
-			file_tree_element->this_directory = save_this_dir;
-			file_tree_element->one_of_the_top_dirs = save_this_dir->one_of_the_top_dirs;
+			file_tree_element->one_of_the_top_dirs = save_up_position->one_of_the_top_dirs;
 			file_tree_element->first_dir_in_chain = save_first_dir;
 			file_tree_element->dirname = dirlist_pointer->name;
 			file_tree_element->dir_location = dirlist_pointer->dir_location;
 			file_tree_element->st_mode = dirlist_pointer->st_mode;
 			file_tree_element->atime = dirlist_pointer->atime;
 			file_tree_element->mtime = dirlist_pointer->mtime;
+			dirlist_pointer->tree_position = file_tree_element;
 		}
 
 		else if (a == limit && limit != 1) {
@@ -209,14 +218,14 @@ DList_of_lists *create_dirs(DList *list, DList_of_lists *file_tree_element, DLis
 				printf("error allocating memory - create_dirs(3)\n");
 				exit(1);
 			}
+			data_copy_info.dlist_of_lists_num++;
 			file_tree_element->next = alloc;
 			file_tree_element = file_tree_element->next;
 			init_to_zero(file_tree_element);
 			file_tree_element->next = NULL;
 			file_tree_element->up = save_up_position;
 			file_tree_element->prev = save_old_position;
-			file_tree_element->this_directory = save_this_dir;
-			file_tree_element->one_of_the_top_dirs = save_this_dir->one_of_the_top_dirs;
+			file_tree_element->one_of_the_top_dirs = save_up_position->one_of_the_top_dirs;
 			file_tree_element->first_dir_in_chain = save_first_dir;
 			file_tree_element->last_dir = 1;
 			file_tree_element->dirname = dirlist_pointer->name;
@@ -224,10 +233,10 @@ DList_of_lists *create_dirs(DList *list, DList_of_lists *file_tree_element, DLis
 			file_tree_element->st_mode = dirlist_pointer->st_mode;
 			file_tree_element->atime = dirlist_pointer->atime;
 			file_tree_element->mtime = dirlist_pointer->mtime;
+			dirlist_pointer->tree_position = file_tree_element;
 			save_old_position = file_tree_element;
 			file_tree_element = file_tree_element->first_dir_in_chain;
 			file_tree_element->last_dir_in_chain = save_old_position;
-			file_tree_element->this_directory->last_dir_in_chain = save_old_position;
 			return file_tree_element;
 		}
 	} /* for loop */
