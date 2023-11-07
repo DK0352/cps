2930a86ee0cd69bfe3fd8ce9b380a1cb019d2f25
Danilo Kovljanić <winterway1726@gmail.com>
2023-11-07 23:50:15 +0100

    various changes related to dlist_ins_next()

--- new_file_location_miss.c
+++ new_file_location_miss.c
@@ -38,24 +38,17 @@ char *new_file_location_miss(DListElmt *main_location, DList_of_lists *new_locat
 
 	size1 = size2 = size3 = 0;
 
-	name = strdup(main_location->name);
-	if (name == NULL) {
-		printf("new_file_location_miss(): strdup() error 1. exiting.\n");
-		exit(1);
-	}
+	name = main_location->name;
 	st_mode = main_location->st_mode;
 	size = main_location->size;
-	dir_location = strdup(main_location->dir_location);
-	if (dir_location == NULL) {
-		printf("new_file_location_miss(): strdup error 2. exiting.\n");
-		exit(1);
-	}
+	dir_location = main_location->dir_location;
+
 	size1 = strlen(new_location->dir_location);
 	size2 = strlen(main_location->name);
 	size3 = size1 + size2 + 2;
 	final_path = malloc(size3);
 	if (final_path == NULL) {
-		printf("new_file_location_miss(): malloc error 1. exiting.\n");
+		printf("new_file_location_miss(): malloc() error 1. exiting.\n");
 		exit(1);
 	}
 	strcpy(final_path,new_location->dir_location);
@@ -63,13 +56,13 @@ char *new_file_location_miss(DListElmt *main_location, DList_of_lists *new_locat
 	strcat(final_path,main_location->name);
 
 	if (options.time_mods == 0)
-		dlist_ins_next(insert_to,insert_to->tail,name,st_mode,size,dir_location,0,final_path,main_location->atime,main_location->mtime);
+		dlist_ins_next(insert_to,insert_to->tail,name,st_mode,size,dir_location,0,final_path,main_location->atime,main_location->mtime,new_location);
 	else if (options.time_mods == 1) {
 		if (options.preserve_a_time == 1)
 			atime = main_location->atime;
 		if (options.preserve_m_time == 1)
 			mtime = main_location->mtime;
-		dlist_ins_next(insert_to,insert_to->tail,name,st_mode,size,dir_location,0,final_path,main_location->atime,main_location->mtime);
+		dlist_ins_next(insert_to,insert_to->tail,name,st_mode,size,dir_location,0,final_path,main_location->atime,main_location->mtime,new_location);
 	}
 
 	return final_path;
