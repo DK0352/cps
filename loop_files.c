e71355b4e23d3f3feb95a22f3ebf0c97e4170189
Danilo Kovljanić <winterway1726@gmail.com>
2024-03-21 21:53:45 +0100

    sym links, surplus files renamed, def scan mode v1

--- loop_files.c
+++ loop_files.c
@@ -34,32 +34,75 @@ int loop_files(DList_of_lists *file_tree_element_a, DList_of_lists *file_tree_el
 {
 	extern struct Data_Copy_Info data_copy_info;
 	extern struct options_menu options;
-	DList	 		*files_a, *files_b;
+	DList	 		*files_a, *files_b, *symlinks_a, *symlinks_b;
 	DListElmt 		*compare_l, *compare_s, *list_l, *list_s;
-	DList_of_lists 		*top_dir_pos, *hold_top;
-	int 			main_mark, same_file_num, filelist_num_a, filelist_num_b; /* isti fajlovi na obe lokacije */
+	DList_of_lists 		*top_dir_pos, *hold_top, *save_a, *save_b;
+	int 			main_mark, same_file_num, filelist_num_a, filelist_num_b, symlink_list_num_a, symlink_list_num_b; 
 	long			size;
 	mode_t			file1_type, file2_type;
+	static int		loop_links;
 
-	if (file_tree_element_a->files != NULL) {
-		files_a = file_tree_element_a->files;
-		filelist_num_a = files_a->num;
+	if (loop_links == 0) {
+		if (file_tree_element_a->files != NULL) {
+			files_a = file_tree_element_a->files;
+			filelist_num_a = files_a->num;
+		}
+		else if (file_tree_element_a->files == NULL) {
+			filelist_num_a = 0;
+		}
+		if (file_tree_element_b->files != NULL) {
+			files_b = file_tree_element_b->files;
+			filelist_num_b = files_b->num;
+		}
+		else if (file_tree_element_b->files == NULL) {
+			filelist_num_b = 0;
+		}
 	}
-	else if (file_tree_element_a->files == NULL) {
-		filelist_num_a = 0;
+	// pretend that the symbolic links are files
+	else if (loop_links == 1) {
+		if (file_tree_element_a->sym_links != NULL) {
+			files_a = file_tree_element_a->sym_links;
+			filelist_num_a = files_a->num;
+		}
+		else if (file_tree_element_a->sym_links == NULL) {
+			filelist_num_a = 0;
+		}
+		if (file_tree_element_b->sym_links != NULL) {
+			files_b = file_tree_element_b->sym_links;
+			filelist_num_b = files_b->num;
+		}
+		else if (file_tree_element_b->sym_links == NULL) {
+			filelist_num_b = 0;
+		}
 	}
-	if (file_tree_element_b->files != NULL) {
-		files_b = file_tree_element_b->files;
-		filelist_num_b = files_b->num;
-	}
-	else if (file_tree_element_b->files == NULL) {
-		filelist_num_b = 0;
+	if (options.ignore_symlinks != 1 && loop_links == 0) {
+		if (file_tree_element_a->sym_links != NULL) {
+			symlinks_a = file_tree_element_a->sym_links;
+			symlink_list_num_a = symlinks_a->num;
+		}
+		else if (file_tree_element_a->sym_links == NULL)
+			symlink_list_num_a = 0;
+		if (file_tree_element_b->sym_links != NULL) {
+			symlinks_b = file_tree_element_b->sym_links;
+			symlink_list_num_b = symlinks_b->num;
+		}
+		else if (file_tree_element_a->sym_links == NULL)
+			symlink_list_num_b = 0;
+		if (symlink_list_num_a != 0 || symlink_list_num_b != 0) {
+			loop_links = 1;
+			//save_a = file_tree_element_a;
+			//save_b = file_tree_element_b;
+			loop_files(file_tree_element_a,file_tree_element_b);
+			//file_tree_element_a = save_a;
+			//file_tree_element_b = save_b;
+			loop_links = 0;
+		}
 	}
 
 	same_file_num = 0;
 
 	/* compare_l is the larger file list, compare_s is the smaller file list. main_mark marks the source (main) directory. this is arranged so that smaller list is compared against  the larger, and
-	the rest is simply added as the files to copy list, or the files surplus list if the same_file_num variable doesn't match the number of files in the source and destination directories. */
+	the rest is simply added as the files to copy list, or the files extraneous list if the same_file_num variable doesn't match the number of files in the source and destination directories. */
 	if (filelist_num_a > 0 && filelist_num_b > 0) {
 		if (filelist_num_a > filelist_num_b) {
 			compare_l = files_a->head; // files_a is file_tree_element_a->files
@@ -85,7 +128,7 @@ int loop_files(DList_of_lists *file_tree_element_a, DList_of_lists *file_tree_el
 		/* This for loop searches for the files with the same name, compares their size, type, and adds them to the appropriate list. for example: if two files with the same name are found, but their
 		size doesn't match, and the file with the main mark is larger, add it to the diff_size_ml_list. (ml is short for main larger). If the file in the destination directory is larger, add it to
 		the diff_size_ms_list (ms is short for main smaller). After the loop finishes, if the number of files in the source or destination doesn't match the same_file_num, the rest of the function 
-		after the loop takes care whether files are added to the copy or the surplus lists. */
+		after the loop takes care whether files are added to the copy or the extraneous lists. */
 		for (compare_l = list_l; compare_l != NULL; compare_l = compare_l->next) {
 			for (compare_s = list_s; compare_s != NULL; compare_s = compare_s->next) {
 				/* Compare file names. If they are same and match isn't 1 (which means that they haven't already been compared, set match to 1, 
@@ -285,18 +328,18 @@ int loop_files(DList_of_lists *file_tree_element_a, DList_of_lists *file_tree_el
 	else if (filelist_num_a == 0 && filelist_num_b > 0) {
 		compare_s = files_b->head;
 		while (compare_s != NULL) {
-			if (data_copy_info.files_surplus_list == NULL) {
-				data_copy_info.files_surplus_list = malloc(sizeof(DList));
-				if (data_copy_info.files_surplus_list != NULL)
-					dlist_init(data_copy_info.files_surplus_list);
+			if (data_copy_info.files_extraneous_list == NULL) {
+				data_copy_info.files_extraneous_list = malloc(sizeof(DList));
+				if (data_copy_info.files_extraneous_list != NULL)
+					dlist_init(data_copy_info.files_extraneous_list);
 				else {
 					printf("loop_files() malloc() error 2-2.\n");
 					exit(1);
 				}
 			}
-			new_file_location_miss(compare_s,file_tree_element_a,data_copy_info.files_surplus_list);
-			data_copy_info.global_files_surplus_num++;
-			data_copy_info.global_files_surplus_size += compare_s->size;
+			new_file_location_miss(compare_s,file_tree_element_a,data_copy_info.files_extraneous_list);
+			data_copy_info.global_files_extraneous_num++;
+			data_copy_info.global_files_extraneous_size += compare_s->size;
 			compare_s->match = 1;
 			compare_s = compare_s->next;
 		} // while() loop
@@ -308,7 +351,7 @@ int loop_files(DList_of_lists *file_tree_element_a, DList_of_lists *file_tree_el
 	// don't go further if filelist_num a and b are same
 	if (filelist_num_a == same_file_num && filelist_num_b == same_file_num)
 		return 0;
-	/*************************************** all files in the directory compared, but same_file_num is not matched, so find the missing/surplus files ************************************************/
+	/*************************************** all files in the directory compared, but same_file_num is not matched, so find the missing/extraneous files ************************************************/
 
 	compare_l = list_l;
 	compare_s = list_s;
@@ -342,18 +385,18 @@ int loop_files(DList_of_lists *file_tree_element_a, DList_of_lists *file_tree_el
 		if (filelist_num_b > same_file_num) {
 			while (compare_s != NULL) {
 				if (compare_s->match != 1) {
-					if (data_copy_info.files_surplus_list == NULL) {
-						data_copy_info.files_surplus_list = malloc(sizeof(DList));
-						if (data_copy_info.files_surplus_list != NULL)
-							dlist_init(data_copy_info.files_surplus_list);
+					if (data_copy_info.files_extraneous_list == NULL) {
+						data_copy_info.files_extraneous_list = malloc(sizeof(DList));
+						if (data_copy_info.files_extraneous_list != NULL)
+							dlist_init(data_copy_info.files_extraneous_list);
 						else {
 							printf("loop_files() malloc() error 3-2.\n");
 							exit(1);
 						}
 					}
-					new_file_location_miss(compare_s,file_tree_element_a,data_copy_info.files_surplus_list);
-					data_copy_info.global_files_surplus_num++;
-					data_copy_info.global_files_surplus_size += compare_s->size;
+					new_file_location_miss(compare_s,file_tree_element_a,data_copy_info.files_extraneous_list);
+					data_copy_info.global_files_extraneous_num++;
+					data_copy_info.global_files_extraneous_size += compare_s->size;
 					compare_s->match = 1;
 					compare_s = compare_s->next;
 				} 
@@ -393,18 +436,18 @@ int loop_files(DList_of_lists *file_tree_element_a, DList_of_lists *file_tree_el
 		if (filelist_num_b > same_file_num) {
 			while (compare_l != NULL) {
 				if (compare_l->match != 1) {
-					if (data_copy_info.files_surplus_list == NULL) {
-						data_copy_info.files_surplus_list = malloc(sizeof(DList));
-						if (data_copy_info.files_surplus_list != NULL)
-							dlist_init(data_copy_info.files_surplus_list);
+					if (data_copy_info.files_extraneous_list == NULL) {
+						data_copy_info.files_extraneous_list = malloc(sizeof(DList));
+						if (data_copy_info.files_extraneous_list != NULL)
+							dlist_init(data_copy_info.files_extraneous_list);
 						else {
 							printf("loop_files() malloc() error 3-4.\n");
 							exit(1);
 						}
 					}
-					new_file_location_miss(compare_l,file_tree_element_a,data_copy_info.files_surplus_list);
-					data_copy_info.global_files_surplus_num++;
-					data_copy_info.global_files_surplus_size += compare_l->size;
+					new_file_location_miss(compare_l,file_tree_element_a,data_copy_info.files_extraneous_list);
+					data_copy_info.global_files_extraneous_num++;
+					data_copy_info.global_files_extraneous_size += compare_l->size;
 					compare_l->match = 1;
 					compare_l = compare_l->next;
 				} 
