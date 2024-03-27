9648d82bdc4b8d036bab315da09f9c2e3c8416ce
Danilo Kovljanić <winterway1726@gmail.com>
2024-03-27 23:44:00 +0100

    more detailed info output, symlinks handled differently

--- errors.h
+++ errors.h
@@ -18,9 +18,11 @@
 */
 
 struct errors_data {
-	short	file_read_error_count;
-	short 	file_write_error_count;
-	short	dir_read_error_count;
-	short	dir_write_error_count;
+	long	file_read_error_count;
+	long 	file_write_error_count;
+	long	dir_read_error_count;
+	long	dir_write_error_count;
+	long	file_perms_error_count;
+	long	dir_perms_error_count;
 	DList *file_read_error, *file_write_error, *dir_read_error, *dir_write_error;
 };
