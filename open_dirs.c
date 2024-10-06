4163c59a4e0d1c9860541ddf5c9e059a11980e52
danilo <winterway1726@gmail.com>
2024-10-06 15:37:55 +0200

    new option ignore added

--- open_dirs.c
+++ open_dirs.c
@@ -35,6 +35,9 @@ int open_dirs(struct thread_struct *thread_data)
 	int data5_val;
 	int dir_entry_init, file_t_init;
 	struct errors_data error;
+	static int checked_for_ignored = 0;
+	int i;
+	DListElmt	*scroll;
 
 	dir_entry_init = 0;
 	file_t_init = 0;
@@ -93,6 +96,7 @@ int open_dirs(struct thread_struct *thread_data)
 
 	path_len = strlen(thread_data->directory);
 
+	if ((options.ignore == 0 && checked_for_ignored == 0) || (options.ignore == 1 && checked_for_ignored == 1)) {
 	for (;;) {
 		if (dir_entry_init != 1) {
 			dir_entry = malloc(sizeof(struct dirent));
@@ -156,9 +160,96 @@ int open_dirs(struct thread_struct *thread_data)
 			dlist_ins_next(thread_data->files, thread_data->files->tail, name, file_t->st_mode, file_t->st_size, location, 0, NULL, file_t->st_atime, file_t->st_mtime,NULL);
 		}
 		else if (S_ISLNK(file_t->st_mode)) {
-			dlist_ins_next(thread_data->sym_links, thread_data->sym_links->tail, name, file_t->st_mode, file_t->st_size, location, data5_val, NULL, file_t->st_atime, file_t->st_mtime,NULL);
+			dlist_ins_next(thread_data->sym_links, thread_data->sym_links->tail, name, file_t->st_mode, file_t->st_size, location, data5_val, NULL, file_t->st_atime, 
+			file_t->st_mtime,NULL);
 		}
 	} // for (;;)
+	} // if (options.ignore == 1 && skip_ignored == 1)
+
+	else if (options.ignore == 1 && checked_for_ignored == 0) {
+	for (;;) {
+		if (dir_entry_init != 1) {
+			dir_entry = malloc(sizeof(struct dirent));
+			if (dir_entry == NULL) {
+				printf("open_dirs(): malloc() dir_entry list error. exiting.\n");
+				exit(1);
+			}
+			dir_entry_init = 1;
+		}
+		errno = 0;
+		dir_entry = readdir(dir);
+		if (dir_entry == NULL) {
+			if (errno == 0) {
+				break;
+			}
+		}
+		if (strcmp(dir_entry->d_name, ".") == 0 || strcmp(dir_entry->d_name, "..") == 0)
+			continue;
+		scroll = options.ignore_list->head;
+		for (i = 0; i < options.ignore_list->num; i++) {
+			if (scroll->match != 1) {
+				if (strcmp(scroll->name,dir_entry->d_name) == 0) {
+					scroll->match = 1;
+					break;
+				}
+			}
+			scroll = scroll->next;
+		}
+		if (scroll != NULL) {
+			if (scroll->match == 1)
+				continue;
+		}
+		// name_len
+		name_len = strlen(dir_entry->d_name)+1; // +1 za '\0'
+		name = malloc(name_len);
+		if (name == NULL) {
+			printf("open_dirs(): malloc error 3\n");
+			exit(1);
+		}
+		strcpy(name,dir_entry->d_name);
+		// complete_size (or complete length to be more correct) which is pathname + name of the file or directory
+		complete_size = path_len + name_len + 1; // +1 za '/'
+		location = malloc(complete_size);
+		if (location == NULL) {
+			printf("open_dirs(): malloc error 4\n");
+			exit(1);
+		}
+		strcpy(location,thread_data->directory);
+		if (strcmp(location,"/") != 0)
+			strcat(location,"/");
+		strcat(location,name);
+
+		if (options.show_read_proc != 0)
+			printf("%s\n", location);
+
+		if (file_t_init != 1) {
+			file_t = malloc(sizeof(struct stat));
+			if (file_t == NULL) {
+				printf("malloc file_t error\n");
+				exit(1);
+			}
+			file_t_init = 1;
+		}
+
+		errno = 0;
+		if (options.stat_f(location,file_t) != 0) {
+			perror("lstat");
+			printf("opendirs(): lstat(): file: %s\n", location);
+			exit(1);
+		}
+		if (S_ISDIR(file_t->st_mode)) {
+			dlist_ins_next(thread_data->directories, thread_data->directories->tail, name, file_t->st_mode, 0, location, 0, NULL, file_t->st_atime, file_t->st_mtime,NULL);
+		}
+		else if (S_ISREG(file_t->st_mode)) {
+			dlist_ins_next(thread_data->files, thread_data->files->tail, name, file_t->st_mode, file_t->st_size, location, 0, NULL, file_t->st_atime, file_t->st_mtime,NULL);
+		}
+		else if (S_ISLNK(file_t->st_mode)) {
+			dlist_ins_next(thread_data->sym_links, thread_data->sym_links->tail, name, file_t->st_mode, file_t->st_size, location, data5_val, NULL, file_t->st_atime, 
+			file_t->st_mtime,NULL);
+		}
+	} // for (;;)
+		checked_for_ignored = 1;
+	}	// else if (options.ignore == 1 && skip_ignored == 0)
 
 	errno = 0;
 	if (closedir(dir) != 0)
